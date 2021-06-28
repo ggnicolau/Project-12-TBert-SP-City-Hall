467e55b75e3e14eb6f8a2feadffc50a98f9e7a50
Guilherme Giuliano Nicolau <79374447+ggnicolau@users.noreply.github.com>
2021-06-28 19:25:35 -0300

    Create TBert.py

---
+++ TBert.py
@@ -0,0 +1,10 @@
+#%%Links
+# BERT with Topic Model    
+https://www.aclweb.org/anthology/2020.acl-main.630.pdf
+https://towardsdatascience.com/topic-modeling-with-bert-779f7db187e6
+https://www.kaggle.com/dskswu/topic-modeling-bert-lda
+https://blog.insightdatascience.com/contextual-topic-identification-4291d256a032
+https://datascience.stackexchange.com/questions/53270/bert-it-is-possible-to-use-it-for-topic-modeling
+https://medium.com/atoti/topic-modeling-on-twitter-using-sentence-bert-8acdad958eb1
+https://medium.com/analytics-vidhya/bert-for-topic-modeling-bert-vs-lda-8076e72c602b
+https://github.com/MilaNLProc/contextualized-topic-models
